3ea2dea0fdddcdb9a1b4dda2ce3adccd0daf2aff
gitDebuger <hygchn04@gmail.com>
2024-08-28 10:51:35 +0800

    Init blog-service and user-blog-service.

---
+++ blog-service/src/test/java/cn/com/spoc/blogservice/BlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.spoc.blogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ user-blog-service/src/main/java/cn/com/spoc/userblogservice/UserBlogServiceApplication.java
@@ -0,0 +1,13 @@
+package cn.com.spoc.userblogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBlogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserBlogServiceApplication.class, args);
+    }
+
+}
